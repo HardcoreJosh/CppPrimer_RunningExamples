b3678a0fac780dce6514f455a302de7ba677498d
Josh <527292215@qq.com>
2017-04-01 21:13:14 +0800

    Blob: class template example

---
+++ Blob/Blob.h
@@ -0,0 +1,53 @@
+#pragma once
+#ifndef BLOB_H
+#define BLOB_H
+
+#include <vector>
+#include <string>
+#include <memory>
+
+template<typename T>
+class Blob {
+public:
+	typedef T value_type;
+	typedef typename std::vector<T>::size_type size_type;
+
+	Blob() :data(std::make_shared<std::vector<T>>()) {}
+	Blob(std::initializer_list<T> il): data(std::make_shared<std::vector<T>>(il)) {}
+
+	size_type size() const {
+		return data->size();
+	}
+
+	void pop_back() {
+		check(0, "index out of range");
+		data->pop_back();
+	}
+	void push_back(const T& val) { data->push_back(val); }
+	void push_back(T&& val) { 
+		data->push_back(std::move(val)); 
+	}
+
+	T& operator[](size_type idx) {
+		check(idx, "index out of range");
+		return (*data)[idx];
+	}
+
+	const T& operator[](size_type idx) const {
+		check(idx, "index out of range");
+		return (*data)[idx];
+	}
+	
+private:
+	std::shared_ptr<std::vector<T>> data;
+	void check(size_type idx, const std::string &msg) const;
+
+};
+
+template<typename T>
+void Blob<T>::check(size_type idx, const std::string &msg) const {
+	if (idx >= data->size())
+		throw std::out_of_range(msg);
+}
+
+#endif
---
+++ Blob/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include "TextQuery.h"
+#include "Blob.h"
+using namespace std;
+
+void fun(string &s)
+{
+
+}
+
+string get_str()
+{
+	return string("");
+}
+
+int main()
+{
+
+	//Blob<string> b{ "1", "2", "3" };
+	//string a = "";
+
+	//fun(a);
+	fun(string(""));
+
+	//fun(b[0]);
+
+
+	/*  TextQuery
+	
+	istringstream is(string("1 2 3\n 2 2 2\n 3 3 3\n"));
+	runQueries(is);
+	*/
+
+}
