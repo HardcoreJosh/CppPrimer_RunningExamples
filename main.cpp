d5b8c5b132dca1f9fbdb142dfdba3cabf9f6ed88
Josh <527292215@qq.com>
2017-04-04 12:50:36 +0800

    StrVec: move semantics example

--- main.cpp
+++ main.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
 #include <string>
 #include <sstream>
-
+#include "TextQuery.h"
+#include "Blob.h"
 using namespace std;
-
-void runQueries(std::istream& in);
-void input(istream& in)
-{
-	string s;
-	while ((in >> s) && s != "q") {
-		cout << s << endl;
-	}
-}
 
 int main()
 {
+	cout << compare<1, 2>() << endl;
+
+
+	/*  TextQuery
 	istringstream is(string("1 2 3\n 2 2 2\n 3 3 3\n"));
 	runQueries(is);
+	*/
 
 }
